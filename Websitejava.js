b5c1cd967952b02e57004cee80461db39d1db81d
Pennyasf <47757186+Pennyasf@users.noreply.github.com>
2019-04-02 18:02:36 +0100

    various changes made to allow the carousel to work

---
+++ Websitejava.js
@@ -0,0 +1,22 @@
+alert("The javascript works")
+
+
+
+// $("button").on("click", function(){
+//   alert("Are you sure you wish to submit?")
+// });
+
+// making the ingredients and methods hide and unhide upon click */
+// function Dropdowntoggle() {
+//   var displaySetting = Dropdowndisplay.style.display;
+//   var Dropdowntogglebutton = document.getElementById("Dropdowntogglebutton");
+// 
+//   if (displaySetting == "block") {
+//     Dropdowndisplay.style.display = "none";
+//     Dropdowntogglebutton.innerHTML = "Show Dropdown";
+//   }
+//   else {
+//     Dropdowndisplay.style.display = "block";
+//     Dropdowntogglebutton.innerHTML = "Hide Dropdown";
+//   }
+// }
