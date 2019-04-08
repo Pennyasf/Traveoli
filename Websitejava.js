abda94169ffc7b75fc4cd5278b61e399ab479df7
Pennyasf <47757186+Pennyasf@users.noreply.github.com>
2019-04-08 18:31:35 +0100

    Revert "New formatting"

--- Websitejava.js
+++ Websitejava.js
@@ -1,9 +1,50 @@
+//test alert for if you dont think the script is working
+// alert("The javascript works")
 
-// Submission button - ok redirects you and cancel keeps you on the page
 
-$("button").click(function (event) {
-  var isGood = confirm('Are you sure you wish to submit?');
-  if (isGood === false) {
-    event.preventDefault();
-  }
+
+ //$("button").on("click", function(){
+   //alert("Are you sure you wish to submit?")
+ //});
+
+ // checkConfirm = confirm("Are you sure you wish to submit?");
+ // if(!checkConfirm){
+ //   $("button").on("click", function(){
+ //     alert("Are you sure you wish to submit?")
+ //   });
+ //     window.close();
+
+
+$("button").click(function() {
+    var isGood=confirm('Are you sure you wish to submit?');
+      if (isGood==true)
+        {
+          alert("This is the if bit");
+          window.location.href="index.html";
+      }
+      else
+        {
+          alert("This is the else bit");
+          window.location.href="asia.html";
+      }
+  
 });
+
+// function confirmation(){
+//    var answer=confirm("Are you sure you want to cancel this record?");
+//    if (answer==true)
+//      {
+//      gsftSubmit(null, g_form.getFormElement(), 'cancel_change'); //MUST call the 'Action name' set in this UI Action
+//    }
+//    else
+//      {
+//      return false;
+//    }
+// }
+// if(typeof window == 'undefined')
+//   {
+//   current.state = 8;
+//   current.update();
+//   action.WhatNextForm.html(current);
+//   gs.addInfoMessage('The current change request has been cancelled.');
+// }
